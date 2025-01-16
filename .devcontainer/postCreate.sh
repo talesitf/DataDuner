71b024f45752e94e383c04b386c772d466925129
Raul Ikeda <rauligs@insper.edu.br>
2025-01-16 14:09:51 -0300

    initial devcontainer files

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Setting up environment..."
+
+# Initialize opam
+opam init -y
+opam switch create 4.14.0
+eval $(opam env)
+
+# Update Cabal and GHCup paths
+echo 'export PATH="$HOME/.ghcup/bin:$HOME/.cabal/bin:$HOME/.local/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
